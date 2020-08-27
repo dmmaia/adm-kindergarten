f04af964aa3d49fc11f8884f7de21718b0c24cce
dmmaia <david_mmaia@hotmail.com>
2020-08-27 20:20:49 -0300

    sign in form created

--- src/components/StudentRegister.js
+++ src/components/StudentRegister.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './components.css';
 
 export default function StudentRegister() {
+
+    function displayLoginBox(){
+        var elementLoginBox = document.getElementById('boxLogin');
+        elementLoginBox.classList.toggle('dontDisplay');
+      }
+
     return (
-        <form className= "registerBox">
+        <form className= "registerBox dontDisplay" id="boxLogin">
+
+            <div className="closeButton " onClick={displayLoginBox}>
+                <div className="first"/>
+                <div className="second"/>
+            </div>
+
             <label></label>
             <input type="text" />
             <label></label>
--- src/pages/Student.js
+++ src/pages/Student.js
@@ -2,8 +2,15 @@ import React from 'react';
 import StudentRegister from '../components/StudentRegister';
 
 export default function Student() {
+
+    function displayLoginBox(){
+        var elementLoginBox = document.getElementById('boxLogin');
+        elementLoginBox.classList.toggle('dontDisplay');
+      }
+
     return (
         <>
+            <button class="btn" onClick={displayLoginBox}>Cadastrar Novo Aluno</button>
             <StudentRegister />
         </>
     )
