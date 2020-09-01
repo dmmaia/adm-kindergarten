401d94580352a43b91ecfed6e0793984e50b5d09
dmmaia <david_mmaia@hotmail.com>
2020-09-01 19:00:18 -0300

    Changing menu item name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="mainHeader">
             <ul>
-                <li><Link to="/alunos" className= "linkHeader" >Estudantes</Link></li>
+                <li><Link to="/alunos" className= "linkHeader" >Alunos</Link></li>
                 <li><Link to="/funcionarios" className= "linkHeader" >Funcionários</Link></li>
                 <li><Link to="/biblioteca" className= "linkHeader" >Biblioteca</Link></li>
                 <li><Link to="/contratos" className= "linkHeader" >Contratos</Link></li>
